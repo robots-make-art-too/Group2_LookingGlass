c74d4b4abb5efb206edd3a8fa47e14bfb977f289
Aspen <thecrimsonmecha@gmail.com>
2022-04-17 21:48:09 -0400

    Update game.js

--- docs/js/game.js
+++ docs/js/game.js
@@ -12,17 +12,26 @@ function createPet() {
     let name = prompt("What will you name your pet?", "Big Floppa");
     pet = new Pet(name);
 
-    spawnNewPet();
+    spawnNewPet('/assets/models/cube.glb');
 
     document.getElementById("petInfo").style.display = "block";
     updateStatDisplay();
 }
 
-function spawnNewPet() {
+function spawnNewPet(source) {
     var scene = document.querySelector('a-scene');
+    var assets = document.querySelector('a-assets');
+
+    // First we need to initialize the model the pet will be using
+    var model = document.createElement('a-asset-item');
+    model.setAttribute('id', 'pet-gltf');
+    model.setAttribute('src', source);
+    assets.appendChild(model);
+
+    // Then, we need to create the pet object from that model
     var pet = document.createElement('a-entity');
-    pet.setAttribute('id', 'cube');
-    pet.setAttribute('gltf-model', document.querySelector('#cube-gltf'));
+    pet.setAttribute('id', 'pet');
+    pet.setAttribute('gltf-model', model);
     pet.setAttribute('position', '0 0 0');
     scene.appendChild(pet);
 }
