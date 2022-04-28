1b2ee5c7b46bb69b6ece9a917fae6f1c36ad49ea
Aspen <thecrimsonmecha@gmail.com>
2022-04-27 21:17:46 -0400

    Test

--- docs/js/game.js
+++ docs/js/game.js
@@ -1,5 +1,5 @@
-const UPDATE_TIME = 15000; // Time in ms to update stats
-const CYCLES_TO_AGE = 25;
+const UPDATE_TIME = 20000; // Time in ms to update stats
+const CYCLES_TO_AGE = 20;
 const IDLE_STAT_DECREASE = 1; // Amount to decrease each stat per update
 var pet; // To be defined when pet is created
 var objectConsumed = false; // So that a marker only spawns an object once each time it is brought into view
